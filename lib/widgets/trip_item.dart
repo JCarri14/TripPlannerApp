b18d76277f3209f7be20f67c6967705330489c94
Jcarri14 <j.carrion@students.salle.url.edu>
2021-01-29 21:10:08 +0100

    Update some files

--- lib/widgets/trip_item.dart
+++ lib/widgets/trip_item.dart
@@ -8,104 +8,6 @@ class TripItem extends StatelessWidget {
       child: Card(
         margin: EdgeInsets.symmetric(vertical: 8),
         elevation: 3.0,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(4.0)),
-              child: SizedBox(
-                height: 140,
-                width: 120,
-                child: Image.network(
-                  'https://scoutmytrip.com/blog/wp-content/uploads/2018/05/Road-trip-with-family-Featured-1024x1024.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            Container(
-              height: 140,
-              width: 200,
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Trip title',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            'From: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('dd/mm/yy')
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'To: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('dd/mm/yy')
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.calendar_today,
-                            ),
-                            SizedBox(
-                              width: 6,
-                            ),
-                            Text('4 days'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.attach_money,
-                            ),
-                            Text('1224'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TripItemImgTop extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Card(
-        margin: EdgeInsets.symmetric(vertical: 8),
-        elevation: 3.0,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -113,9 +15,9 @@ class TripItemImgTop extends StatelessWidget {
               borderRadius: BorderRadius.vertical(top: Radius.circular(4.0)),
               child: SizedBox(
                 height: 140,
-                width: double.infinity,
+                width: 300,
                 child: Image.network(
-                  'https://scoutmytrip.com/blog/wp-content/uploads/2018/05/Road-trip-with-family-Featured-1024x1024.jpg',
+                  'https://static.toiimg.com/thumb/66440952/road-trip.jpg?width=1200&height=900',
                   fit: BoxFit.cover,
                 ),
               ),
@@ -130,9 +32,10 @@ class TripItemImgTop extends StatelessWidget {
                   subtitle: Text('From: dd/mm/yy To: dd/mm/yy')),
             ),
             Container(
+              padding: EdgeInsets.only(bottom: 8,left: 16, right: 16),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Row(
                           children: [
@@ -145,6 +48,9 @@ class TripItemImgTop extends StatelessWidget {
                             Text('4 days'),
                           ],
                         ),
+                        SizedBox(
+                              width: 6,
+                            ),
                         Row(
                           children: [
                             Icon(
