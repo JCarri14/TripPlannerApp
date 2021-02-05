05c8d90a8a0e2824f5b72fbb7c26cac8b1cbb405
danielcasadofauli <daniel.casado@students.salle.url.edu>
2021-02-05 02:47:07 +0100

    feat: Trip plan completed

--- lib/models/tripPlan.dart
+++ lib/models/tripPlan.dart
@@ -1,12 +1,15 @@
 import 'package:trip_planner_app/models/flight/trip.dart';
 import 'package:trip_planner_app/models/hotel/hotel.dart';
+import 'package:trip_planner_app/models/tourDay.dart';
 class TripPlan {
   final Trip departureTrip;
   final Trip returnTrip;
   final Hotel hotel;
+  final List<TourDay> tourDaysList;
 
   TripPlan({this.departureTrip,
   this.returnTrip,
-  this.hotel});
+  this.hotel,
+  this.tourDaysList});
 
 }
