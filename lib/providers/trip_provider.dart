a00dc74b8a3ba4af6e42045ce79abe215b559385
Jcarri14 <j.carrion@students.salle.url.edu>
2021-02-05 10:48:05 +0100

    update: trip provider class

--- lib/providers/trip_provider.dart
+++ lib/providers/trip_provider.dart
@@ -1,5 +1,101 @@
 import 'package:flutter/widgets.dart';
 
+import '../models/tripPlan.dart';
+import '../models/geo/city.dart';
+import '../models/flight/flight.dart';
+import '../models/flight/airport.dart';
+
+
 class TripManager with ChangeNotifier {
-  
+    TripPlan _tripPlan;
+    City _originCity;
+    City _destinationCity;
+    DateTime _destinationDay;
+    DateTime _returnDay;
+    int budget;
+
+    TripManager() {
+      _tripPlan = new TripPlan();
+      _originCity = new City();
+      _destinationCity = new City();
+    }
+
+    void saveOriginCity(City city) {
+      _originCity = city;
+      notifyListeners();
+    } 
+
+    void saveDestinationCity(City city) {
+      _destinationCity = city;
+      notifyListeners();
+    }
+
+    void saveOriginAirport(Airport originAirport) {
+      _tripPlan.originAirport = originAirport;
+      notifyListeners();
+    }
+
+    void saveDestinationAirport(Airport destinationAirport) {
+      _tripPlan.destinationAirport = destinationAirport;
+      notifyListeners();
+    }    
+
+    void saveDepartureFlight(Flight df) {
+      _tripPlan.departureFlight = df;
+      notifyListeners();
+    }
+
+    void saveReturnFlight(Flight rf) {
+      _tripPlan.returnFlight = rf;
+      notifyListeners();
+    }
+
+    City get originCity {
+      return _originCity;
+    }
+
+    City get destinationCity {
+      return _destinationCity;
+    }
+
+    String get destinationDate {
+      return _destinationDay.toString().split(" ")[0];
+    }
+
+    String get returnDate {
+      return _returnDay.toString().split(" ")[0];
+    }
+
+    Airport get originAirport {
+      return _tripPlan.originAirport;
+    }
+
+    Airport get destinationAirport {
+      return _tripPlan.destinationAirport;
+    }
+
+    Flight get departureFlight {
+      return _tripPlan.departureFlight;
+    }
+
+    Flight get returnFlight {
+      return _tripPlan.returnFlight;
+    }
+
+    void saveDestinationDay(DateTime destinationDay) {
+      this._destinationDay = destinationDay;
+      notifyListeners();
+    }
+
+    void saveReturnDay(DateTime returnDay) {
+      this._returnDay = returnDay;
+      notifyListeners();
+    }
+
+    void saveBudget(int budget) {
+      this.budget = budget;
+      notifyListeners();
+    }
+
+    
 }
