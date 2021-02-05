3ba5de22b3906994429f6e27e4156f65992aed92
danielcasadofauli <daniel.casado@students.salle.url.edu>
2021-02-05 02:47:30 +0100

    feat: Error de commit

--- lib/models/tourDay.dart
+++ lib/models/tourDay.dart
@@ -1,6 +1,5 @@
 import 'package:trip_planner_app/models/event/event.dart';
-import 'package:trip_planner_app/models/flight/trip.dart';
-import 'package:trip_planner_app/models/hotel/hotel.dart';
+
 class TourDay {
   final List<Event> dayEventsList;
 
