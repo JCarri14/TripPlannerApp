26eb42df75ba81fea1c9c89bad8f7e0a9e64fb23
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-17 01:08:18 +0200

    feat: add routes file

---
+++ lib/src/config/routes/routes.dart
@@ -0,0 +1,16 @@
+const String homeRoute = "/";
+const String loginRoute = "/login";
+const String registerRoute = "/register";
+const String searchRoute = "/search";
+const String mapRoute = "/map";
+
+//FLIGHTS
+const String flightRoute = "/flight/:id";
+const String flightSearchRoute = "/flight-search";
+
+//HOTELS
+const String hotelRoute = "/hotel/:id";
+
+//TRIPS
+const String tripRoute = "trip/:id";
+const String tripCreationRoute = "/create/trip"; 
