3244601fa2ac168bede259b5d855770acba45338
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-17 01:13:16 +0200

    feat: add exportation files for services and pages

---
+++ lib/src/network/services.dart
@@ -0,0 +1,1 @@
+export "./services/flight_service.dart";
---
+++ lib/src/ui/pages.dart
@@ -0,0 +1,20 @@
+//HOME
+export "./pages/home/home_page.dart";
+
+//AUTH
+export "./pages/auth/login/login_page.dart";
+export "./pages/auth/register/register_page.dart";
+
+//SEARCH
+export "./pages/search/search_page.dart";
+
+//TRIP
+
+//FLIGHT
+
+
+//HOTEL
+
+//OTHER
+export "./pages/other/unkown_screen.dart";
+export "./pages/other/unkown_screen.dart";
