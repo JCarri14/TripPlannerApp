4b256ac997383b7d799e72f715e49061771a2459
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-17 01:08:02 +0200

    feat: add main helpers, models, providers file exportation

---
+++ lib/src/business_logic/models.dart
@@ -0,0 +1,24 @@
+//USER
+export "models/user/user_model.dart";
+
+//TRIP
+export 'models/trip/trip_model.dart';
+
+//CITY + COUNTRY + REGION
+export 'models/geolocation/city_model.dart';
+export 'models/geolocation/country_model.dart';
+export 'models/geolocation/region_model.dart';
+
+//FLIGHT
+export 'models/transport/flight_model.dart';
+
+//AIRPORT
+export 'models/transport/airport_model.dart';
+
+//HOTEL
+export 'models/hotel/hotel_model.dart';
+export 'models/hotel/landmark.dart';
+
+//EVENT
+export 'models/event/event_model.dart';
+export 'models/event/event_categories.dart';
