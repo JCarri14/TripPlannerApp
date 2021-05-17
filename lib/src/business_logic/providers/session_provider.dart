13a638415e72ba815b3b9476d3a2cf0ad5b469b0
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-17 09:03:30 +0200

    feat: add session provider

---
+++ lib/src/business_logic/providers/session_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../storage/local_storage.dart';
+import '../models.dart';
+
+class SessionProvider {
+  final _storage = new LocalStorage();
+  bool isLoggedIn = false;
+  String authToken = "";
+
+  Future<void> loadData() async {
+    authToken = await _storage.readData("auth-token");
+  }
+
+  Future<void> writeAuthToken(String? value) async {
+    isLoggedIn = value != null ? true : false;
+    await _storage.writeData("auth-token", value!);
+  }
+
+  Future<String> readAuthToken() async {
+    if (authToken == "") {
+      authToken = await _storage.readData("auth-token"); 
+    }
+    return authToken;
+  }
+
+  Future<void> writeData(String key, String value) async {
+    await _storage.writeData(key, value);
+  }
+
+  Future<String> readData(String key) async {
+    return await _storage.readData(key);
+  }
+
+  Future<void> removeData(String key) async {
+    await _storage.deleteData(key);
+  }
+
+  Future<dynamic> readAllData() async {
+    return await _storage.readAllData();
+  }
+}
