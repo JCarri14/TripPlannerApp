e4590cb6ac8f01bb4e44a2589567683fdd007111
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-18 04:01:12 +0200

    feat: update form field widget

--- lib/src/ui/widgets/forms/form_field.dart
+++ lib/src/ui/widgets/forms/form_field.dart
@@ -5,23 +5,30 @@ class CustomFormField extends StatefulWidget {
   final String title;
   final String hintText;
   final IconData prefixIcon;
+  final String inputValue;
   final TextInputType inputType;
   final bool obscureText;
   final Function(String) validator;
+  final Function onChanged;
+  final Function onTapHandler;
 
   CustomFormField({
     this.title = "Input", 
     this.hintText = "Enter value", 
-    this.prefixIcon = Icons.add,
+    this.prefixIcon,
+    this.inputValue = "",
     this.inputType = TextInputType.name,
     this.obscureText = false,
-    this.validator = baseValidator});
+    this.validator = baseValidator,
+    this.onChanged,
+    this.onTapHandler
+  });
 
   @override
-  Custom_FormFieldState createState() => Custom_FormFieldState();
+  CustomFormFieldState createState() => CustomFormFieldState();
 }
 
-class Custom_FormFieldState extends State<CustomFormField> {
+class CustomFormFieldState extends State<CustomFormField> {
 
   final inputController = TextEditingController();
 
@@ -33,38 +40,92 @@ class Custom_FormFieldState extends State<CustomFormField> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Text(
-              widget.title,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                fontSize: Theme.of(context).textTheme.headline5?.fontSize,
-              ),),
-          ),
-          SizedBox(height: 8,),
-          TextFormField(
-            controller: inputController,
-            validator: (value) { return widget.validator(value!);},
-            obscureText: widget.obscureText,
-            decoration: InputDecoration(
-              hintText: widget.hintText,
-              prefixIcon: Icon(widget.prefixIcon),
-              suffixIcon: inputController.text.isEmpty ? 
-                Container(width: 0)
-                : IconButton(
-                  icon: Icon(Icons.close, color: Colors.blue,),
-                  onPressed: () => inputController.clear(),
-                ),
-              border: OutlineInputBorder(),
+    
+    return widget.onTapHandler != null ? 
+    InkWell(
+      onTap: widget.onTapHandler,
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Text(
+                widget.title,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: Theme.of(context).textTheme.headline5?.fontSize,
+                ),),
+            ),
+            SizedBox(height: 8,),
+            Container(
+              height: 45,
+              child: TextFormField(
+                controller: inputController..text = widget.inputValue,
+                validator: (value) { return widget.validator(value);},
+                obscureText: widget.obscureText,
+                decoration: InputDecoration(
+                  hintText: widget.hintText,
+                  prefixIcon: widget.prefixIcon != null ? Icon(widget.prefixIcon): null,
+                  suffixIcon: inputController.text.isEmpty ? 
+                    Container(width: 0)
+                    : IconButton(
+                      icon: Icon(Icons.close, color: Colors.blue,),
+                      onPressed: () => inputController.clear(),
+                    ),
+                  border: OutlineInputBorder(),
+                  ),
+                keyboardType: widget.inputType,
+                onTap: widget.onTapHandler,
               ),
-              keyboardType: widget.inputType,
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    );
+    ):
+    Container(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Text(
+                widget.title,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: Theme.of(context).textTheme.headline5?.fontSize,
+                ),),
+            ),
+            SizedBox(height: 8,),
+            Container(
+              height: 45,
+              child: TextFormField(
+                controller: inputController..text,
+                validator: (value) { return widget.validator(value);},
+                onChanged: (value) {
+                  widget.onChanged(value);
+                  setState(() {
+                    print(value);
+                    inputController..text = value;
+                  });
+                },
+                obscureText: widget.obscureText,
+                decoration: InputDecoration(
+                  hintText: widget.hintText,
+                  prefixIcon: widget.prefixIcon != null ? Icon(widget.prefixIcon): null,
+                  suffixIcon: inputController.text.isEmpty ? 
+                    Container(width: 0)
+                    : IconButton(
+                      icon: Icon(Icons.close, color: Colors.blue,),
+                      onPressed: () => inputController.clear(),
+                    ),
+                  border: OutlineInputBorder(),
+                  ),
+                keyboardType: widget.inputType,
+              ),
+            )
+          ],
+        ),
+      );
   }
 }
