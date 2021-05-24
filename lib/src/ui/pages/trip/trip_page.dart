ed3ab512c151d12c6d40646f7da7d80de7f48306
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-24 14:21:35 +0200

    feat: update trip page

--- lib/src/ui/pages/trip/trip_page.dart
+++ lib/src/ui/pages/trip/trip_page.dart
@@ -3,24 +3,40 @@ import 'package:provider/provider.dart';
 
 //WIDGETS
 import "../../widgets/containers/base_scrollable_container.dart";
+import "./trip_events_carousel.dart";
 
 //PROVIDERS
 import "../../../business_logic/providers.dart";
 
+//MODELS
+import "../../../business_logic/models.dart";
+
+//ROUTES
+import "../../../config/routes/routes.dart";
+
+class TripPageArguments {
+  final Trip trip;
+  final bool saveMode;
+
+  TripPageArguments({this.trip, this.saveMode});
+}
 
 class TripPage extends StatefulWidget {
+
   @override
   _TripPageState createState() => _TripPageState();
 }
 
 class _TripPageState extends State<TripPage> {
   final scaffoldState = GlobalKey<ScaffoldState>();
-
-  TripCreationProvider tripManager;
+  AppProvider appProvider;
 
   @override
   Widget build(BuildContext context) {  
-    tripManager = Provider.of<TripCreationProvider>(context);
+    appProvider = Provider.of<AppProvider>(context);
+
+    final args = ModalRoute.of(context).settings.arguments as TripPageArguments;
+    print(args.trip.toJson());
 
     return Scaffold(
       key: scaffoldState,
@@ -30,7 +46,119 @@ class _TripPageState extends State<TripPage> {
       body: BaseScrollableContainer(
         child: Column(
           children: [
-
+            SizedBox(height: 16,),
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    width: double.infinity,
+                    child: Text(
+                      "Origin", 
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),                 
+                  ListTile(
+                    title: Text("City"),
+                    subtitle: Text(args.trip.originCity.toString()),
+                  ),
+                  ListTile(
+                    title: Text("Airport"),
+                    subtitle: Text(args.trip.originAirport.placeName),
+                  ),
+                  ListTile(
+                    title: Text("Flight Ticket"),
+                    subtitle: Row(
+                      children: [
+                        Text(args.trip.departureFlight.airlineName),
+                        SizedBox(width: 8),
+                        Text(args.trip.departureFlight.minPrice.toString() + " €", textAlign: TextAlign.end, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54)),
+                      ]
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16,),
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    width: double.infinity,
+                    child: Text(
+                      "Destination", 
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ), 
+                  ListTile(
+                    title: Text("City"),
+                    subtitle: Text(args.trip.destinationCity.toString()),
+                  ),
+                  ListTile(
+                    title: Text("Airport"),
+                    subtitle: Text(args.trip.destinationAirport.placeName),
+                  ),
+                  ListTile(
+                    title: Text("Flight Ticket"),
+                    subtitle: Row(
+                      children: [
+                        Text(args.trip.returnFlight.airlineName),
+                        SizedBox(width: 8),
+                        Text(args.trip.returnFlight.minPrice.toString() + " €", textAlign: TextAlign.end, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54)),
+                      ]
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16,),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              width: double.infinity,
+              child: Text(
+                "Events", 
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+            ),
+            TripEventsCarousel(),
+            SizedBox(height: 16,),
+            args.saveMode ? 
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              height: 45,
+              width: double.infinity,
+              child: ElevatedButton(
+                autofocus: false,
+                clipBehavior: Clip.none,
+                child: Text("Continue"),
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor
+                ),
+                onPressed: () {
+                  appProvider.addTrip(args.trip);
+                  Navigator.of(context).popAndPushNamed(
+                    homeRoute);
+                },
+              )
+            ):
+            Container(),
+            SizedBox(height: 16,),
           ]
         ),
       ),
