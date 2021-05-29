b6b74f8be5c4d3a2b5c517b1ce4724a9f8a0ee00
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-29 12:52:07 +0200

    feat: update hotel search page

--- lib/src/ui/pages/hotel/hotel_search_page.dart
+++ lib/src/ui/pages/hotel/hotel_search_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
+import 'package:lottie/lottie.dart';
 
 //MODELS
 import "../../../business_logic/models.dart";
@@ -7,8 +9,12 @@ import "../../../business_logic/models.dart";
 //PROVIDERS
 import "../../../business_logic/providers.dart";
 
+//ROUTES
+import "../../../config/routes/routes.dart";
+
 //OTHER
 import '../../../network/api/api_response.dart';
+import "./hotel_search_item.dart";
 
 class HotelSearchPage extends StatefulWidget {
   @override
@@ -17,90 +23,55 @@ class HotelSearchPage extends StatefulWidget {
 
 class _HotelSearchPageState extends State<HotelSearchPage> {
   
-  HotelProvider hotelBloc;
+  HotelProvider hotelProvider;
   TripCreationProvider tripManager;
 
-  Widget _buildStreamBuilder(BuildContext ctx) {
-    return StreamBuilder(
-          stream: hotelBloc.hotelListStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              switch (snapshot.data.status) {
-                case Status.LOADING:
-                return Container(
-                  child: Center(
-                    child: Text('Loading...'),
-                  )
-                );
-                case Status.COMPLETED:
-                List<Hotel> hotels = snapshot.data.data ?? [];
-                return Container(
-                  width: double.infinity,
-                  child: ListView.builder(
-                    itemCount: hotels.length,
-                    itemBuilder: (_, index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(),
-                      padding: EdgeInsets.all(8),
-                        child: Material(
-                        elevation: 4.0,
-                        child: Container(
-                          child: Row(
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(hotels[index].name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                                  Text(hotels[index].address)
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.attach_money),
-                                  Text(hotels[index].pricePerNight.toString() + "€")
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.star),
-                                  Text(hotels[index].starRating.toString())
-                                ],
-                              )
-                            ],
-                          )
-                        )
-                      ),
-                    ); //CardItem(hotel: hotels[index]);
-                  },)
-                );
-              }
-            }
-            return Container();
-          },
-    );
-  }
+  //LOGIC
+  bool firstFetch = false;
+  List<Widget> hotelsList = [];
 
   @override
   Widget build(BuildContext context) {
     tripManager = Provider.of<TripCreationProvider>(context);
-    hotelBloc = new HotelProvider();
+    hotelProvider = Provider.of<HotelProvider>(context);
 
     String posId = tripManager.positionId;
     String chckIn = tripManager.destinationDate;
     String chckOut = tripManager.returnDate;
     
-    hotelBloc.fetchHotels(context, posId, chckIn, chckOut, '500');
+    if (!firstFetch) {
+      Timer(Duration(milliseconds: 500), () {
+        hotelProvider.fetchHotels(context, posId, chckIn, chckOut, '500');
+      });
+      setState(() {
+        firstFetch = true;
+      });
+    }
 
+    if (hotelProvider.isLoaded) {
+      hotelsList = hotelProvider.hotels.map((h) => 
+        HotelSearchItem(
+          hotel: h,
+          isSelected: hotelProvider.selectedHotel != null ? hotelProvider.selectedHotel.name == h.name:false,
+          onTapHandler: () {
+            hotelProvider.saveSelectedHotel(h);
+          },
+        )
+      ).toList();
+    }
+    
     return Scaffold(
       appBar: AppBar(
         title: Text("Hotel Search")
       ),
       body: SafeArea(
         child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 8),
           child: Column(children: [
             SizedBox(height: 8,),
             Material(
               elevation: 8.0,
+              color: Colors.white,
               borderRadius: BorderRadius.circular(8),
               child: ListTile(
                 title: Text('Nothing like a good place to stay!'),
@@ -109,8 +80,40 @@ class _HotelSearchPageState extends State<HotelSearchPage> {
             ),
             SizedBox(height: 16,),
             Expanded(
-              child: _buildStreamBuilder(context)
+              child: hotelProvider.isLoaded ?
+                ListView.builder(
+                  itemCount: hotelsList.length,
+                  itemBuilder: (ctx, idx) =>
+                    hotelsList[idx]
+                  ):
+                Container(
+                  child: Center(
+                    child: Lottie.asset("assets/circle-loader.json"),
+                  )
+                ),
             ),
+            SizedBox(height: 16,),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              height: 45,
+              width: double.infinity,
+              child: ElevatedButton(
+                autofocus: false,
+                clipBehavior: Clip.none,
+                child: Text("Continue"),
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor
+                ),
+                onPressed: () {
+                  tripManager.saveHotel(
+                    hotelProvider.selectedHotel
+                  );
+                  Navigator.of(context).pushNamed(
+                    eventSelectionRoute, arguments: false);
+                },
+              )
+            ),
+            SizedBox(height: 8,),
           ],)
         )
       )
